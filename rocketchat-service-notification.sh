7d7f7585accb41d8fc5966f9bff4a11fe7dbb4be
BackIsBachus <backisbachus@larez.fr>
2016-04-21 22:59:41 +0200

    Change separator before SERVICEOUTPUT

--- rocketchat-service-notification.sh
+++ rocketchat-service-notification.sh
@@ -21,5 +21,5 @@ else
 fi
 
 #Send message to Rocket.Chat
-PAYLOAD="payload={\"text\": \"${ICON} ${NOTIFICATIONTYPE} <http://${ICINGA_HOSTNAME}/icingaweb2/monitoring/host/services?host=${HOSTNAME}|${HOSTDISPLAYNAME}> (<http://${ICINGA_HOSTNAME}/icingaweb2/monitoring/service/show?host=${HOSTNAME}&service=${SERVICEDESC}|${SERVICEDISPLAYNAME}>): ${SERVICESTATE} - ${SERVICEOUTPUT}\"}"
+PAYLOAD="payload={\"text\": \"${ICON} ${NOTIFICATIONTYPE} <http://${ICINGA_HOSTNAME}/icingaweb2/monitoring/host/services?host=${HOSTNAME}|${HOSTDISPLAYNAME}> (<http://${ICINGA_HOSTNAME}/icingaweb2/monitoring/service/show?host=${HOSTNAME}&service=${SERVICEDESC}|${SERVICEDISPLAYNAME}>): ${SERVICESTATE} | ${SERVICEOUTPUT}\"}"
 curl --connect-timeout 30 --max-time 60 -s -S -X POST --data-urlencode "${PAYLOAD}" "${ROCKETCHAT_WEBHOOK_URL}"
