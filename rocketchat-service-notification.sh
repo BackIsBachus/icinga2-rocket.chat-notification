57299938d2b4a86c62017ea41b443684a4fb47a7
BackIsBachus <backisbachus@larez.fr>
2016-04-21 15:47:33 +0200

    Change name from slack to rocketchat

---
+++ rocketchat-service-notification.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+ICINGA_HOSTNAME="<YOUR_ICINGAWEB2_HOSTNAME>"
+ROCKETCHAT_WEBHOOK_URL="<YOUR_SLACK_WEBHOOK_INTEGRATION_URL>"
+
+#Set the message icon based on ICINGA service state
+if [ "$SERVICESTATE" = "CRITICAL" ]
+then
+    ICON=":bomb:"
+elif [ "$SERVICESTATE" = "WARNING" ]
+then
+    ICON=":warning:"
+elif [ "$SERVICESTATE" = "OK" ]
+then
+    ICON=":beer:"
+elif [ "$SERVICESTATE" = "UNKNOWN" ]
+then
+    ICON=":question:"
+else
+    ICON=":white_medium_square:"
+fi
+
+#Send message to Slack
+PAYLOAD="payload={\"text\": \"${ICON} HOST: <http://${ICINGA_HOSTNAME}/icingaweb2/monitoring/host/services?host=${HOSTNAME}|${HOSTDISPLAYNAME}>   SERVICE: <http://${ICINGA_HOSTNAME}/icingaweb2/monitoring/service/show?host=${HOSTNAME}&service=${SERVICEDESC}|${SERVICEDISPLAYNAME}>  STATE: ${SERVICESTATE}\"}"
+
+curl --connect-timeout 30 --max-time 60 -s -S -X POST --data-urlencode "${PAYLOAD}" "${ROCKETCHAT_WEBHOOK_URL}"
